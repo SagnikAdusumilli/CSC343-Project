17689e8b558487fd82f9077ae9341888cc59f146
snau <shimmy.nauenberg@mail.utoronto.ca>
2021-11-18 14:57:04 -0500

    write up first draft

--- data-clean/creating tables.sql
+++ data-clean/creating tables.sql
@@ -6,7 +6,7 @@ CHECK (VALUE IN ('Bike Rack', 'Angled Bike Rack', 'Bike Corral',
 
 DROP TABLE IF EXISTS bikeshop CASCADE;
 CREATE TABLE bikeShop (
-stName TEXT not null,
+stName TEXT not null references Streets(stName),
 stNumber INTEGER not null,
 hasRental BOOLEAN not null,
 PRIMARY KEY ( stName, stNumber));
@@ -15,7 +15,7 @@ DROP TABLE IF EXISTS ParkingSpots CASCADE;
 CREATE TABLE ParkingSpots(
 	ID INTEGER not null,
 	stNumber INTEGER not null,
-	stName TEXT not null,
+	stName TEXT not null references Streets(stName),
 	spotType spottype,
 	capacity INTEGER not null,
 	PRIMARY KEY (ID));
@@ -23,8 +23,8 @@ CREATE TABLE ParkingSpots(
 	
 DROP TABLE IF EXISTS redLightCamera CASCADE;	
 CREATE TABLE redLightCamera(
- stName1 TEXT not null,
- stName2 TEXT not null,
+ stName1 TEXT not null references Streets(stName),
+ stName2 TEXT not null references Streets(stName),
  PRIMARY KEY(stName1, stName2));
 
 \COPY bikeshop FROM 'bike_shop.csv' with csv header
